02a0c75bc379b36a852730e6af383dba450bf717
Andrej Kabachnik <andrej_ka@gmx.de>
2016-12-14 15:53:32 +0100

    FIX default values for checkboxes

--- Template/Elements/euiCheckBox.php
+++ Template/Elements/euiCheckBox.php
@@ -10,10 +10,10 @@ class euiCheckbox extends euiInput {
 		$output = '	<div style="width: calc(100% + 2px); height: 100%; display: inline-block; text-align:left;">
 						<input type="checkbox" value="1" 
 								id="' . $this->get_id() . '_checkbox"
-								onchange="$(\'#' . $this->get_id() . '\').val(this.checked); console.log($(\'#' . $this->get_id() . '\')); console.log(this.checked);"' . '
-								' . ($this->get_widget()->get_value() ? 'checked="checked" ' : '') . '
+								onchange="$(\'#' . $this->get_id() . '\').val(this.checked);"' . '
+								' . ($this->get_value_with_defaults() ? 'checked="checked" ' : '') . '
 								' . ($this->get_widget()->is_disabled() ? 'disabled="disabled"' : '') . ' />
-						<input type="hidden" name="' . $this->get_widget()->get_attribute_alias() . '" id="' . $this->get_id() . '" value="' . $this->get_widget()->get_value() . '" />
+						<input type="hidden" name="' . $this->get_widget()->get_attribute_alias() . '" id="' . $this->get_id() . '" value="' . $this->get_value_with_defaults() . '" />
 					</div>';
 		return $this->build_html_wrapper_div($output);
 	}
