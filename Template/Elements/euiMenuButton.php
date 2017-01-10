6525127b83deae490417294f06b0aa1b48d5d609
Stefan Leupold <stefan.leupold@salt-solutions.de>
2017-01-10 17:48:39 +0100

    #56445 Alignment von Buttons im Dialog

--- Template/Elements/euiMenuButton.php
+++ Template/Elements/euiMenuButton.php
@@ -57,7 +57,9 @@ class euiMenuButton extends euiAbstractElement {
 			</div>
 			';
 		
-		if ($this->get_widget()->get_input_widget() instanceof Dialog) {
+		if ($this->get_widget()->get_input_widget() instanceof Dialog && !$this->get_widget()->get_parent() instanceof Dialog) {
+			// Hier wird versucht zu unterscheiden wo sich der Knopf befindet. Der Wrapper wird nur benoetigt
+			// wenn er sich in einem Dialog befindet, aber nicht als Knopf im Footer, sondern im Inhalt.
 			$output = $this->build_html_wrapper_div($output);
 		}
 		
