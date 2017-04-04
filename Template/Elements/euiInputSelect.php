bb54d8ff9a481221ce9471212198ffdbe68de7de
Andrej Kabachnik <andrej_ka@gmx.de>
2017-04-04 11:47:01 +0200

    FIX ulti_select in InputSelect widget

--- Template/Elements/euiInputSelect.php
+++ Template/Elements/euiInputSelect.php
@@ -1,5 +1,15 @@
 <?php
 namespace exface\JEasyUiTemplate\Template\Elements;
+use exface\Core\Widgets\InputSelect;
+
+/**
+ * The InputSelect widget will be rendered into a combobox in jEasyUI.
+ * 
+ * @method InputSelect get_widget()
+ * 
+ * @author Andrej Kabachnik
+ *
+ */
 class euiInputSelect extends euiInput {
 	
 	protected function init(){
@@ -8,20 +18,22 @@ class euiInputSelect extends euiInput {
 	}
 	
 	function generate_html(){
-		/* @var $widget \exface\Core\Widgets\InputSelect */
 		$widget = $this->get_widget();
 		$options = '';
 		foreach ($widget->get_selectable_options() as $value => $text){
+			if (!($this->get_widget()->get_multi_select() && count($this->get_widget()->get_values()) > 1)){
+				$selected = strcasecmp($this->get_value_with_defaults(), $value) == 0 ? true : false;
+			}
 			$options .= '
-					<option value="' . $value . '"' . (strcasecmp($this->get_value_with_defaults(), $value) == 0 ? ' selected="selected"' : '') . '>' . $text . '</option>';
+					<option value="' . $value . '"' . ($selected ? ' selected="selected"' : '') . '>' . $text . '</option>';
 		}
 
 		$output = '	<select style="height: 100%; width: 100%;" class="easyui-' . $this->get_element_type() . '" 
-						name="' . $widget->get_attribute_alias() . '"  
+						name="' . $widget->get_attribute_alias() . ($widget->get_multi_select() ? '[]' : '') . '"  
 						id="' . $this->get_id() . '"  
 						' . ($widget->is_required() ? 'required="true" ' : '') . '
 						' . ($widget->is_disabled() ? 'disabled="disabled" ' : '') . '
-						' . ($this->build_js_data_options() ? 'data_options="' . $this->build_js_data_options() . '" ' : '') . '>
+						' . ($this->build_js_data_options() ? 'data-options="' . $this->build_js_data_options() . '" ' : '') . '>
 						' . $options . '
 					</select>
 					';
@@ -34,11 +46,13 @@ class euiInputSelect extends euiInput {
 	}
 	
 	public function build_js_value_getter(){
-		return "$('#" . $this->get_id() . "')." . $this->get_element_type() . "('getValue')";
+		return "$('#" . $this->get_id() . "')." . $this->get_element_type() . "('" . ($this->get_widget()->get_multi_select() ? 'getValues' : 'getValue') . "')";
 	}
 	
 	public function build_js_data_options(){
-		return ($this->get_widget()->get_multi_select() ? 'multiple: true' : '');
+		return "panelHeight: 'auto'"
+				. ($this->get_widget()->get_multi_select() ? ", multiple:true" : '')
+				. ($this->get_widget()->get_multi_select() && count($this->get_widget()->get_values()) > 1 ? ", value:['" . implode("'" . $this->get_widget()->get_multi_select_value_delimiter() . "'", $this->get_widget()->get_values()) . "']" : '');
 	}
 }
 ?>
