3c159a7369ccd58acbebdbdf2dfcd392064353bb
Andrej Kabachnik <andrej_ka@gmx.de>
2017-04-07 17:03:06 +0200

    NEW DataMatrix widget. Old one renamed to DataMatrixOld

--- Template/Elements/euiDataMatrix.php
+++ Template/Elements/euiDataMatrix.php
@@ -1,6 +1,7 @@
 <?php
 namespace exface\JEasyUiTemplate\Template\Elements;
-use exface\Core\CommonLogic\DataSheets\DataColumn;
+
+use exface\Core\Widgets\DataColumnTransposed;
 
 class euiDataMatrix extends euiDataTable {
 	private $label_values = array();
@@ -8,145 +9,160 @@ class euiDataMatrix extends euiDataTable {
 	protected function init(){
 		parent::init();
 		$this->set_element_type('datagrid');
+		$this->build_js_transposer();
+		
 	}
 	
-	function generate_js(){
-		$widget = $this->get_widget();
-		$output = '';
-		
-		if ($this->is_editable()){
-			foreach ($this->get_editors() as $editor){
-				$output .= $editor->build_js_inline_editor_init();
+	protected function build_js_transposer(){
+		$visible_cols = array();
+		$data_cols = array();
+		$data_cols_totlas = array();
+		$label_cols = array();
+		foreach ($this->get_widget()->get_columns() as $col){
+			if ($col instanceof DataColumnTransposed){
+				$data_cols[] = $col->get_data_column_name();
+				$label_cols[] = $col->get_label_attribute_alias();
+				if ($col->get_footer()){
+					$data_cols_totlas[$col->get_data_column_name()] = $col->get_footer();
+				}
+			} elseif (!$col->is_hidden()){
+				$visible_cols[] = $col->get_data_column_name();
 			}
 		}
+		$visible_cols= "'" . implode("','", $visible_cols) . "'";
+		$data_cols= "'" . implode("','", $data_cols) . "'";
+		$label_cols= "'" . implode("','", array_unique($label_cols)) . "'";
+		$data_cols_totlas = json_encode($data_cols_totlas);
 		
-		// get the standard params for grids
-		$grid_head = $this->build_js_init_options();
-		
-		// instantiate the data grid
-		$output .= '$("#' . $this->get_id() . '").' . $this->get_element_type() . '({' . $grid_head . '});';
-		
-		// doSearch function for the filters
-		if ($widget->has_filters()){
-			foreach($widget->get_filters() as $fnr => $fltr){
-				$fltr_impl = $this->get_template()->get_element($fltr, $this->get_page_id());
-				$output .= $fltr_impl->generate_js();
-				$fltrs[] = '"fltr' . str_pad($fnr, 2, 0, STR_PAD_LEFT) . '_' . urlencode($fltr->get_attribute_alias()) . '": ' . $fltr_impl->build_js_value_getter();
+		$transpose_js = <<<JS
+if (data.transposed) return data;
+
+var dataCols = [ {$data_cols} ];
+var dataColsTotals = {$data_cols_totlas};
+var labelCols = [ {$label_cols} ];
+var rows = data.rows;
+var cols = $(this).datagrid('options').columns;
+var colsNew = [];
+var colsTransposed = {};
+for (var i=0; i<cols.length; i++){
+	var newColRow = [];
+	for (var j=0; j<cols[i].length; j++){
+		var fld = cols[i][j].field;
+		if (dataCols.indexOf(fld) > -1){
+			data.transposing = 1;
+			colsTransposed[fld] = cols[i][j];
+		} else if (labelCols.indexOf(fld) > -1) {
+			// Add a subtitle column to show a caption for each subrow if there are multiple
+			if (dataCols.length > 1){
+				var newCol = $.extend(true, {}, cols[i][j]);
+				newCol.field = fld+'_subtitle';
+				newCol.title = '';
+				newCol.align = 'right';
+				newColRow.push(newCol);
 			}
-			// build JS for the search function
-			$output .= '
-						function ' . $this->build_js_function_prefix() . 'doSearch(){
-							$("#' . $this->get_id() . '").' . $this->get_element_type() . '("load",{' . implode(', ', $fltrs) . ', resource: "' . $this->get_page_id() . '", element: "' .  $this->get_widget()->get_id() . '"});
-						}';
-		}
-		
-		return $output;
-	}
-	
-	/**
-	 * This special data source renderer fetches data according to the filters an reorganizes the rows and column to fit the matrix.
-	 * It basically transposes the data column (data_column_id) using values of the label column (label_column_id) as new column headers.
-	 * The other columns remain untouched.
-	 * @see \exface\JEasyUiTemplate\Template\Elements\grid::build_js_data_source()
-	 */
-	public function build_js_data_source(){
-		/* @var $widget \exface\Core\Widgets\DataMatrix  */
-		$widget = $this->get_widget();
-		$visible_columns = array();
-		$output = '';
-		$result = array();
-				
-		// create data sheet to fetch data
-		$ds = $this->get_template()->get_workbench()->data()->create_data_sheet($this->get_meta_object());
-		// add columns
-		foreach ($widget->get_columns() as $col){
-			$ds->get_columns()->add_from_expression($col->get_attribute_alias(), $col->get_data_column_name(), $col->is_hidden());
-			if (!$col->is_hidden()) $visible_columns[] = $col->get_data_column_name();
-		}
-		// add the filters
-		foreach ($widget->get_filters() as $fw){
-			if (!is_null($fw->get_value())){
-				$ds->add_filter_from_string($fw->get_attribute_alias(), $fw->get_value());
-			}
-		}
-		// add the sorters
-		foreach ($widget->get_sorters() as $sort){
-			$ds->get_sorters()->add_from_string($sort->attribute_alias, $sort->direction);
-		}
-		// add aggregators
-		foreach ($widget->get_aggregations() as $aggr){
-			$ds->get_aggregators()->add_from_string($aggr);
-		}
-		
-		// get the data
-		$ds->data_read();
-		$label_col = $widget->get_label_column();
-		$data_col = $widget->get_data_column();
-		foreach ($ds->get_rows() as $nr => $row){
-			$new_row_id = null;
-			$new_row = array();
-			$new_col_val = null;
-			$new_col_id = null;
-			foreach ($row as $fld => $val){
-				
-				if ($fld === $label_col->get_data_column_name()){
-					$new_col_id = $val;
-					// TODO we probably need a special parameter for sorting labels!
-					if (!in_array($val, $this->label_values)) $this->label_values[] = $val;
-				} elseif ($fld === $data_col->get_data_column_name()){
-					$new_col_val = $val; 
-				} elseif (in_array($fld, $visible_columns)) {
-					$new_row_id .= $val;
-					$new_row[$fld] = $val;
+			// Create a column for each value if the label column
+			var labels = [];
+			for (var l=0; l<rows.length; l++){
+				if (labels.indexOf(rows[l][fld]) == -1){
+					labels.push(rows[l][fld]);
 				}
 			}
-			if (!is_array($result[$new_row_id])){
-				$result[$new_row_id] = $new_row;
+			for (var l=0; l<labels.length; l++){
+				var newCol = $.extend(true, {}, cols[i][j]);
+				newCol.field = rows[l][fld];
+				newCol.title = '<span title="'+$(newCol.title).text()+' '+rows[l][fld]+'">'+rows[l][fld]+'</title>';
+				// No header sorting if multiple sublines (not clear, what to sort!)
+				if (dataCols.length > 1){
+					newCol.sortable = false;
+				}
+				newColRow.push(newCol);
 			}
-			$result[$new_row_id][$new_col_id] = $new_col_val;
-		}
-		
-		$output = "data: [";
-		foreach ($result as $row){
-			$output .= "{";
-			foreach ($row as $fld => $val){
-				$output .= '"' . $this->clean_id($fld) . '": "' . str_replace('"', '\"', $val) .'",';
+			// Create a totals column if there are totals
+			if (dataColsTotals !== {}){
+				var totals = [];
+				for (var t in dataColsTotals){
+					var tfunc = dataColsTotals[t];
+					if (totals.indexOf(tfunc) == -1){
+						var newCol = $.extend(true, {}, cols[i][j]);
+						newCol.field = fld+'_'+tfunc;
+						newCol.title = tfunc;
+						newCol.align = 'right';
+						newColRow.push(newCol);
+						totals.push(tfunc);
+					}
+				}
 			}
-			$output = substr($output, 0, -1);
-			$output .= '},';
+		} else {
+			newColRow.push(cols[i][j]);
 		}
-		$output = substr($output, 0, -1);
-		$output .= ']';
-		return $output;
+	}
+	colsNew.push(newColRow);
+}
+
+if (data.transposing){
+	var newRows = [];
+	var newRowsObj = {};
+	var visibleCols = [ {$visible_cols} ];
+	for (var i=0; i<rows.length; i++){
+		var newRowId = '';
+		var newRow = {};
+		var newColVals = {};
+		var newColId = '';
+		for (var fld in rows[i]){
+			var val = rows[i][fld];
+			if (labelCols.indexOf(fld) > -1){
+				newColId = val;
+				newColGroup = fld;
+			} else if (dataCols.indexOf(fld) > -1){
+				newColVals[fld] = val; 
+			} else if (visibleCols.indexOf(fld) > -1) {
+				newRowId += val;
+				newRow[fld] = val;
+			}
+		}
+		for (var fld in newColVals){
+			if (newRowsObj[newRowId+fld] == undefined){
+				newRowsObj[newRowId+fld] = $.extend(true, {}, newRow);
+			}
+			newRowsObj[newRowId+fld][newColId] = newColVals[fld];
+			newRowsObj[newRowId+fld][newColGroup+'_subtitle'] = '<i>'+colsTransposed[fld].title+'</i>';
+			if (dataColsTotals[fld] != undefined){
+				var newVal = parseFloat(newColVals[fld]);
+				var oldVal = newRowsObj[newRowId+fld][newColGroup+'_'+dataColsTotals[fld]];
+				oldVal = oldVal ? oldVal : 0;
+				switch (dataColsTotals[fld]){
+					case 'SUM':
+						newRowsObj[newRowId+fld][newColGroup+'_'+dataColsTotals[fld]] = oldVal + newVal; 
+						break;
+					case 'MAX':
+						newRowsObj[newRowId+fld][newColGroup+'_'+dataColsTotals[fld]] = oldVal < newVal ? newVal : oldVal; 
+						break;
+					case 'MIN':
+						newRowsObj[newRowId+fld][newColGroup+'_'+dataColsTotals[fld]] = oldVal > newVal ? newVal : oldVal; 
+						break;
+					case 'COUNT':
+						newRowsObj[newRowId+fld][newColGroup+'_'+dataColsTotals[fld]] = oldVal + 1; 
+						break;
+					// TODO add more totals
+				}
+			}
+		}
+	}
+	for (var i in newRowsObj){
+		newRows.push(newRowsObj[i]);
+	}
+	data.rows = newRows;
+	data.transposed = 1;
+	data.transposing = 0;
+	$(this).datagrid({columns: colsNew});
+}
+	
+
+return data;
+				
+JS;
+		$this->add_load_filter_script($transpose_js);
 	}
 	
-	/**
-	 * This special column renderer for the matrix replaces the column specified by label_column_id with a set of new columns for
-	 * every unique value in the column specified by data_column_id. The new columns retain most properties of the replaced label column.
-	 * @see \exface\JEasyUiTemplate\Template\Elements\grid::build_js_init_options_columns()
-	 */
-	public function build_js_init_options_columns(array $column_groups = null){
-		$widget = $this->get_widget();
-		$cols = $this->get_widget()->get_columns();
-		$new_cols = $widget->get_page()->create_widget('DataColumnGroup', $widget);
-		foreach ($cols as $id => $col){
-			if ($col->get_id() === $this->get_widget()->get_data_column_id()){
-				// replace the data column with a new set of columns for each possible label
-				foreach ($this->label_values as $label){
-					$new_col = clone($col);
-					$new_col->set_data_column_name(DataColumn::sanitize_column_name($label));
-					$new_col->set_caption($label);
-					$new_col->set_sortable(false);
-					$new_cols->add_column($new_col);
-				}
-			} elseif($col->get_id() === $this->get_widget()->get_label_column_id()) {
-				// doing nothing here makes the label column disapear
-			} else {
-				$new_cols->add_column($col);
-			}
-		}
-		
-		return parent::build_js_init_options_columns(array($new_cols));
-	}
 }
 ?>
