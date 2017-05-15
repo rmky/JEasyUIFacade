48f2e4a0462189334089da9b299e8cb99e8625d5
Andrej Kabachnik <andrej_ka@gmx.de>
2017-05-15 17:18:12 +0200

    FIX tabs can now be disabled

--- Template/Elements/euiTab.php
+++ Template/Elements/euiTab.php
@@ -1,10 +1,22 @@
 <?php
 namespace exface\JEasyUiTemplate\Template\Elements;
+
+use exface\Core\Widgets\Tab;
+
+/**
+ * 
+ * @author Andrej Kabachnik
+ *
+ * @method Tab get_widget()
+ */
 class euiTab extends euiPanel {
 	
 	function generate_html(){
+		
+		$options = $this->get_widget()->is_hidden() || $this->get_widget()->is_disabled() ? 'disabled:true' : '';
+		
 		$output = <<<HTML
-	<div title="{$this->get_widget()->get_caption()}" class="grid">
+	<div title="{$this->get_widget()->get_caption()}" data-options="{$options}" class="grid">
 		{$this->build_html_for_children()}
 	</div>
 HTML;
--- Template/Elements/euiTabs.php
+++ Template/Elements/euiTabs.php
@@ -1,8 +1,18 @@
 <?php
 namespace exface\JEasyUiTemplate\Template\Elements;
+
+use exface\Core\Widgets\Tabs;
+
+/**
+ * 
+ * @author Andrej Kabachnik
+ * 
+ * @method Tabs get_widget()
+ *
+ */
 class euiTabs extends euiContainer {
 	
-	function generate_html(){
+	public function generate_html(){
 		$output = <<<HTML
 	<div id="{$this->get_id()}" class="easyui-tabs" data-options="fit:true,border:false">
 		{$this->build_html_for_children()}
@@ -10,5 +20,19 @@ class euiTabs extends euiContainer {
 HTML;
 		return $output;
 	}
+	
+	/*public function generate_js(){
+		$js = parent::generate_js();
+		
+		foreach ($this->get_widget()->get_tabs() as $nr => $tab){
+			if ($tab->is_hidden()){
+				$js .= <<<JS
+
+			$('#{$this->get_id()}').tabs('close', {$nr});
+
+JS;
+			}
+		}
+	}*/
 }
 ?>
