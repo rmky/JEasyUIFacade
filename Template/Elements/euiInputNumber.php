969e2409a6ea6eef38f07a4b2e8296f03e9d8cc7
Andrej Kabachnik <andrej_ka@gmx.de>
2017-10-16 08:12:36 +0200

    FIX flexible precision for InputNumber widgets

--- Template/Elements/euiInputNumber.php
+++ Template/Elements/euiInputNumber.php
@@ -1,6 +1,14 @@
 <?php
 namespace exface\JEasyUiTemplate\Template\Elements;
 
+use exface\Core\Widgets\InputNumber;
+
+/**
+ * @method InputNumber getWidget()
+ * 
+ * @author Andrej Kabachnik
+ *
+ */
 class euiInputNumber extends euiInput
 {
 
@@ -12,14 +20,35 @@ class euiInputNumber extends euiInput
 
     protected function buildJsDataOptions()
     {
+        $widget = $this->getWidget();
         $output = parent::buildJsDataOptions();
-        if ($output) {
-            $output .= ', ';
-        }
+        $output = ($output ? ',' : '') . $output;
         
-        $output .= "precision: '" . $this->getWidget()->getPrecision() . "'
-					, decimalSeparator: ','
-				";
-        return $output;
+        $formatter = $this->buildJsFormatter('value');
+        
+        $output .= "precision: " . ($widget->getPrecisionMax() ? $widget->getPrecisionMax() : 10)
+                . ", decimalSeparator: '{$widget->getDecimalSeparator()}'"
+                . ($formatter ?  ", formatter:function(value){" . $formatter . "}" : "")
+				;
+        return trim($output, ',');
+    }
+    
+    protected function buildJsFormatter($value_js_var)
+    {
+        $widget = $this->getWidget();
+        if (is_null($widget->getPrecisionMax()) || $widget->getPrecisionMin() === $widget->getPrecisionMax()){
+            $js = <<<JS
+            
+            if ({$value_js_var} !== undefined && {$value_js_var} !== ''){
+    			var {$value_js_var} = $.fn.numberbox.defaults.formatter.call(this,{$value_js_var});
+                {$value_js_var} = {$value_js_var}.replace(/{$widget->getDecimalSeparator()}/g, '.');
+    			var number = parseFloat({$value_js_var});
+                {$value_js_var} = number.toString().replace(/\./g, '{$widget->getDecimalSeparator()}');
+    			return {$value_js_var};
+            }
+
+JS;
+        }
+        return $js;
     }
 }
