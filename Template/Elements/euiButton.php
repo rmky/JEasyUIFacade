8d7312233aedce78d80939ef7bd88e878ce1a0e2
Stefan Leupold <stefan.leupold@salt-solutions.de>
2017-11-09 18:35:30 +0100

    FIX UiPageInstaller Fehler bei nicht vorhandenen Seiten

--- Template/Elements/euiButton.php
+++ Template/Elements/euiButton.php
@@ -114,7 +114,7 @@ class euiButton extends euiAbstractElement
         /* @var $prefill_link \exface\Core\CommonLogic\WidgetLink */
         $prefill = '';
         if ($prefill_link = $this->getAction()->getPrefillWithDataFromWidgetLink()) {
-            if ($prefill_link->getPage()->is($widget->getPage())) {
+            if ($widget->getPage()->is($prefill_link->getPageAlias())) {
                 $prefill = ", prefill: " . $this->getTemplate()->getElement($prefill_link->getWidget())->buildJsDataGetter($this->getAction());
             }
         }
