220be9d67d913fcdd06eb15a876f77f8f6dedb49
Andrej Kabachnik <andrej_ka@gmx.de>
2017-11-23 07:23:43 +0100

    REFACTOR use HtmlImageTrait

--- Template/Elements/euiImage.php
+++ Template/Elements/euiImage.php
@@ -2,6 +2,7 @@
 namespace exface\JEasyUiTemplate\Template\Elements;
 
 use exface\Core\Widgets\Image;
+use exface\Core\Templates\AbstractAjaxTemplate\Elements\HtmlImageTrait;
 
 /**
  *
@@ -12,32 +13,6 @@ use exface\Core\Widgets\Image;
  */
 class euiImage extends euiText
 {
-
-    function generateHtml()
-    {
-        $style = '';
-        if (! $this->getWidget()->getWidth()->isUndefined()) {
-            $width = ' width="' . $this->getWidth() . '"';
-        }
-        if (! $this->getWidget()->getHeight()->isUndefined()) {
-            $height = ' height="' . $this->getHeight() . '"';
-        }
-        
-        switch ($this->getWidget()->getAlign()) {
-            case EXF_ALIGN_CENTER:
-                $style .= 'margin-left: auto; margin-right: auto;';
-                break;
-            case EXF_ALIGN_RIGHT:
-                $style .= 'float: right';
-        }
-        
-        $output = '<img src="' . $this->getWidget()->getUri() . '"' . $width . $height . ' style="' . $style . '" />';
-        return $output;
-    }
-
-    function generateJs()
-    {
-        return '';
-    }
+    use HtmlImageTrait;
 }
 ?>
