ffeb62fa85c04fd8d6566c363ed1750550b5db52
Andrej Kabachnik <andrej_ka@gmx.de>
2018-02-06 11:16:00 +0100

    FIX column formatters

--- Template/Elements/euiData.php
+++ Template/Elements/euiData.php
@@ -621,7 +621,19 @@ HTML;
         
         // Formatter option
         if ($formatter_js) {
-            $options = "formatter: function({$js_var_value},{$js_var_row},{$js_var_index}){try {return " . $formatter_js . ";} catch (e) {return {$js_var_value}; console.warn('Cannot apply decorator to column {$col->getDataColumnName()}');} }";
+            $options = <<<JS
+
+                        formatter: function({$js_var_value},{$js_var_row},{$js_var_index}){
+
+                            try {
+                                return {$formatter_js};
+                            } catch (e) {
+                                console.warn('Cannot apply decorator to column {$col->getDataColumnName()}');
+                                return {$js_var_value}; 
+                            } 
+
+                        }
+JS;
         }
         
         // Styler option
