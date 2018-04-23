38feb8dc8fa73cb95a60138271bd1011459711e3
aka <andrej.kabachnik@salt-solutions.de>
2018-04-23 13:31:12 +0200

    NEW configurable markup styling with GitHub-style defaults

---
+++ Templates/Elements/euiMarkdown.php
@@ -0,0 +1,28 @@
+<?php
+namespace exface\JEasyUiTemplate\Templates\Elements;
+
+class euiMarkdown extends euiHtml
+{    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\Templates\AbstractAjaxTemplate\Elements\AbstractJqueryElement::buildHtmlHeadTags()
+     */
+    public function buildHtmlHeadTags()
+    {
+        $includes = parent::buildHtmlHeadTags();   
+        $includes[] = '<link href="' . $this->getTemplate()->buildUrlToSource('SOURCES.MARKDOWN.CSS') . '" rel="stylesheet">';
+        return $includes;
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\JEasyUiTemplate\Templates\Elements\euiValue::buildCssElementClass()
+     */
+    public function buildCssElementClass()
+    {
+        return parent::buildCssElementClass() . ' markdown-body';
+    }
+}
+?>
