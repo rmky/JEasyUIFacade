1a86ae2591f0f517c53310566f28e8e16e286f97
Andrej Kabachnik <andrej_ka@gmx.de>
2018-06-18 11:24:38 +0200

    NEW options for DataTrees

--- Templates/Elements/euiDataTree.php
+++ Templates/Elements/euiDataTree.php
@@ -18,23 +18,57 @@ class euiDataTree extends euiDataTable
     {
         parent::init();
         $this->setElementType('treegrid');
+        
+        if ($this->getWidget()->getTreeLeafIdColumnId() !== null) {
+            $leafIdCol = $this->getWidget()->getColumn($this->getWidget()->getTreeLeafIdColumnId());
+            if (! $leafIdCol->getDataColumnName()) {
+                $leafIdCol->setDataColumnName('_leafId');
+            }
+        }
     }
 
     public function buildJsInitOptionsHead()
-    {
+    {        
+        $widget = $this->getWidget();
+        $leafIdColumnName = $widget->getTreeLeafIdColumn()->getDataColumnName();
+        
         if ($this->isEditable()) {
             $this->addOnExpand('
 					if (row){
-						var rows = $(this).' . $this->getElementType() . '("getChildren", row.' . $this->getWidget()->getUidColumn()->getDataColumnName() . ');
+						var rows = $(this).' . $this->getElementType() . '("getChildren", row.' . $leafIdColumnName . ');
 						for (var i=0; i<rows.length; i++){
-							$(this).' . $this->getElementType() . '("beginEdit", rows[i].' . $this->getWidget()->getUidColumn()->getDataColumnName() . ');
+							$(this).' . $this->getElementType() . '("beginEdit", rows[i].' . $leafIdColumnName . ');
 						}
 					}
 					');
         }
-        $grid_head = parent::buildJsInitOptionsHead() . '
-                        , treeField: "' . $this->getWidget()->getTreeColumn()->getDataColumnName() . '"
+        
+        if (($leafIdDelim = $widget->getTreeLeafIdConcatenate()) !== null) {
+            $calculatedIdField = ', idField: "_leafId"';
+            $leafIdCalcScript = 'data.rows[row]["_leafId"] = (parentId ? parentId+"' . $leafIdDelim . '" : "")+data.rows[row]["' . $widget->getUidColumn()->getDataColumnName() . '"];';
+        }
+        
+        $grid_head = parent::buildJsInitOptionsHead() . $calculatedIdField . '
+                        , treeField: "' . $widget->getTreeColumn()->getDataColumnName() . '"
                         , lines: false
+                        , loadFilter: function(data, parentId) {
+                            
+                            var row = 0;
+                            var rowCnt = data.rows.length;
+                            var field, parentRow;
+                            
+                            for (row=0; row<rowCnt; row++) {
+                                if (parentId !== null) {
+                                    data.rows[row]["_parentId"] = parentId;
+                                }
+                                ' . $leafIdCalcScript . '
+                            }
+
+                            return data;
+                        }
+                        , onLoadSuccess: function(row, data) {
+                            ' . $this->getOnLoadSuccess() . '
+                        }
                         ' . ($this->buildJsOnExpandScript() ? ', onExpand: function(row){' . $this->buildJsOnExpandScript() . '}' : '');
         
         return $grid_head;
@@ -46,23 +80,28 @@ class euiDataTree extends euiDataTable
         /* @var $widget \exface\Core\Widgets\DataTree */
         $widget = $this->getWidget();
         foreach ($result['rows'] as $nr => $row) {
-            if ($row[$widget->getTreeFolderFlagAttributeAlias()]) {
-                // $result['rows'][$nr]['state'] = $row[$this->getWidget()->getTreeFolderFlagAttributeAlias()] ? 'closed' : 'open';
-                $result['rows'][$nr]['state'] = 'closed';
-                // Dirty hack to remove zero numeric values on folders, because they are easily assumed to be sums
-                foreach ($row as $fld => $val) {
-                    if (is_numeric($val) && intval($val) == 0) {
-                        $result['rows'][$nr][$fld] = '';
+            if ($widget->hasTreeFolderFlag()) {
+                if ($row[$widget->getTreeFolderFlagAttributeAlias()]) {
+                    // $result['rows'][$nr]['state'] = $row[$this->getWidget()->getTreeFolderFlagAttributeAlias()] ? 'closed' : 'open';
+                    $result['rows'][$nr]['state'] = 'closed';
+                    // Dirty hack to remove zero numeric values on folders, because they are easily assumed to be sums
+                    foreach ($row as $fld => $val) {
+                        if (is_numeric($val) && intval($val) == 0) {
+                            $result['rows'][$nr][$fld] = '';
+                        }
                     }
+                } else {
+                    $result['rows'][$nr]['state'] = 'open';
                 }
+                
+                unset($result['rows'][$nr][$this->getWidget()->getTreeFolderFlagAttributeAlias()]);
             } else {
-                $result['rows'][$nr]['state'] = 'open';
+                $result['rows'][$nr]['state'] = 'closed';
             }
             
-            unset($result['rows'][$nr][$this->getWidget()->getTreeFolderFlagAttributeAlias()]);
-            if ($result['rows'][$nr][$widget->getTreeParentIdAttributeAlias()] != $widget->getTreeRootUid()) {
+            /*if ($result['rows'][$nr][$widget->getTreeParentIdAttributeAlias()] != $widget->getTreeRootUid()) {
                 $result['rows'][$nr]['_parentId'] = $result['rows'][$nr][$widget->getTreeParentIdAttributeAlias()] ? $result['rows'][$nr][$widget->getTreeParentIdAttributeAlias()] : 0;
-            }
+            }*/
         }
         
         $result['footer'][0][$this->getWidget()->getTreeColumn()->getDataColumnName()] = '';
@@ -93,13 +132,13 @@ class euiDataTree extends euiDataTable
     protected function buildJsOnBeforeLoadScript($js_var_param = 'param', $js_var_row = 'row')
     {
         return parent::buildJsOnBeforeLoadScript($js_var_param) . <<<JS
-                    var nodeId = {$js_var_param}['id'];
-                    if (nodeId) {
+                    var parentId = {$js_var_param}['id'];
+                    if (parentId) {
                         if ({$js_var_param}['data'] !== undefined && {$js_var_param}['data']['filters'] !== undefined && {$js_var_param}['data']['filters']['conditions'] !== undefined) {
                             var conditions = {$js_var_param}['data']['filters']['conditions'];
                             for (var c in conditions) {
                                 if (conditions[c]['expression'] == '{$this->getWidget()->getTreeParentIdAttributeAlias()}') {
-                                    {$js_var_param}['data']['filters']['conditions'][c]['value'] = nodeId;
+                                    {$js_var_param}['data']['filters']['conditions'][c]['value'] = row['{$this->getWidget()->getTreeFolderFilterColumn()->getDataColumnName()}'];
                                 }
                             }
                         }
