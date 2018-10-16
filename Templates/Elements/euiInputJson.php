22910666906eec63d9d5b1b66be4cebb926a238d
Andrej Kabachnik <andrej_ka@gmx.de>
2018-10-16 18:32:08 +0200

    FIX widget InputJson could not be disabled

--- Templates/Elements/euiInputJson.php
+++ Templates/Elements/euiInputJson.php
@@ -41,14 +41,38 @@ class euiInputJson extends euiInputText
         $init_value = $this->getValueWithDefaults() ? $this->getId() . '_JSONeditor.set(' . $this->getWidget()->getValue() . ');' : '';
         $script = <<<JS
             var {$this->getId()}_JSONeditor = new JSONEditor(document.getElementById("{$this->getId()}"), {
-                            					mode: 'tree',
-                           						modes: ['code', 'form', 'text', 'tree', 'view']
+                            					mode: {$this->buildJsEditorModeDefault()},
+                           						modes: {$this->buildJsEditorModes()}
                         					});
             {$init_value}
             {$this->getId()}_JSONeditor.expandAll();
             $('#{$this->getId()}').parents('.exf-input').children('label').css('vertical-align', 'top');
 JS;
         return $script;
+    }
+    
+    /**
+     * 
+     * @return string
+     */
+    protected function buildJsEditorModes() : string
+    {
+        if ($this->getWidget()->isDisabled()) {
+            return "['view']";
+        }
+        return "['code', 'tree']";
+    }
+    
+    /**
+     * 
+     * @return string
+     */
+    protected function buildJsEditorModeDefault() : string
+    {
+        if ($this->getWidget()->isDisabled()) {
+            return "'view'";
+        }
+        return "'tree'";
     }
     
     /**
