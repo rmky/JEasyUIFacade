9dfdd86075324aae63d820fdad9f8714f9dd0b46
Andrej Kabachnik <andrej_ka@gmx.de>
2018-11-14 16:00:09 +0100

    FIX Message widget outside of MessageList

--- Templates/Elements/euiMessage.php
+++ Templates/Elements/euiMessage.php
@@ -2,6 +2,8 @@
 namespace exface\JEasyUiTemplate\Templates\Elements;
 
 use exface\Core\Templates\AbstractAjaxTemplate\Elements\HtmlMessageTrait;
+use exface\Core\Widgets\Message;
+use exface\Core\Widgets\MessageList;
 
 class euiMessage extends euiText
 {
@@ -9,7 +11,11 @@ class euiMessage extends euiText
     
     public function buildHtml()
     {
-        return $this->buildHtmlMessage();
+        if ($this->getWidget()->getParent() instanceof MessageList) {
+            return $this->buildHtmlMessage();
+        } else {
+            return $this->buildHtmlGridItemWrapper($this->buildHtmlMessage());
+        }
     }
 }
 ?>
