2323a568479c08b65a34ed80c7d299103f7cb285
Andrej Kabachnik <andrej_ka@gmx.de>
2018-12-07 12:06:26 +0100

    FIX dashboard gird back to 3 columns by default

--- Templates/Elements/euiDashboard.php
+++ Templates/Elements/euiDashboard.php
@@ -3,5 +3,13 @@ namespace exface\JEasyUiTemplate\Templates\Elements;
 
 class euiDashboard extends euiPanel
 {
+    /**
+     *
+     * {@inheritDoc}
+     * @see \exface\JEasyUiTemplate\Templates\Elements\euiWidgetGrid::getDefaultColumnNumber()
+     */
+    public function getDefaultColumnNumber()
+    {
+        return $this->getTemplate()->getConfig()->getOption("WIDGET.DASHBOARD.COLUMNS_BY_DEFAULT");
+    }
 }
-?>
