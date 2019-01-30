8bb9d2ba1d692afea488b8c0f95bc26aceb101bc
aka <andrej.kabachnik@salt-solutions.de>
2019-01-30 18:17:39 +0100

    REFACTOR to use JqueryInputTrait

--- Templates/Elements/euiInputHidden.php
+++ Templates/Elements/euiInputHidden.php
@@ -1,50 +1,56 @@
 <?php
 namespace exface\JEasyUiTemplate\Templates\Elements;
 
+use exface\Core\Templates\AbstractAjaxTemplate\Elements\JqueryInputTrait;
+
 class euiInputHidden extends euiInput
 {
-
+    use JqueryInputTrait;
+    
     protected function init()
     {
         parent::init();
         $this->setElementType('hidden');
     }
-
+    
+    /**
+     *
+     * {@inheritDoc}
+     * @see \exface\JEasyUiTemplate\Templates\Elements\euiInput::buildHtml()
+     */
     function buildHtml()
     {
-        $output = '<input type="hidden" 
-								name="' . $this->getWidget()->getAttributeAlias() . '" 
-								value="' . $this->getValueWithDefaults() . '" 
-								id="' . $this->getId() . '" />';
-        return $output;
+        return $this->buildHtmlInput('hidden');
     }
-
+    
+    /**
+     *
+     * {@inheritDoc}
+     * @see \exface\JEasyUiTemplate\Templates\Elements\euiInput::buildJs()
+     */
     function buildJs()
     {
         $output .= $this->buildJsEventScripts();
         return $output;
     }
-
+    
+    /**
+     *
+     * {@inheritDoc}
+     * @see \exface\JEasyUiTemplate\Templates\Elements\euiInput::buildJsValueSetterMethod()
+     */
     function buildJsValueSetterMethod($value)
     {
         return 'val(' . $value . ').trigger("change")';
     }
-
+    
+    /**
+     *
+     * {@inheritDoc}
+     * @see \exface\Core\Templates\AbstractAjaxTemplate\Elements\AbstractJqueryElement::buildJsValueGetterMethod()
+     */
     function buildJsValueGetterMethod()
     {
         return 'val()';
-    }
-
-    /**
-     *
-     * {@inheritdoc}
-     * @see \exface\Core\Templates\AbstractAjaxTemplate\Elements\AbstractJqueryElement::buildJsValidator()
-     */
-    function buildJsValidator()
-    {
-        if ($this->getWidget()->isRequired()) {
-            return '(' . $this->buildJsValueGetter() . ' === "" ? false : true)';
-        } 
-        return 'true';
     }
 }
