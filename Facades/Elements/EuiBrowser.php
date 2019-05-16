8b63200da1d1280d0f85c88bea0f51e164b51173
Andrej Kabachnik <andrej_ka@gmx.de>
2019-05-16 08:27:45 +0200

    REFACTOR improved EuiBrowser with new trait structure

--- Facades/Elements/EuiBrowser.php
+++ Facades/Elements/EuiBrowser.php
@@ -5,6 +5,16 @@ class EuiBrowser extends EuiAbstractElement
 {
     use HtmlBrowserTrait;
     
+    public function buildHtml()
+    {
+        return $this->buildHtmlIFrame();
+    }
+    
+    public function buildJs()
+    {
+        return '';
+    }
+    
     public function buildCssElementStyle()
     {
         return 'width: 100%; height: calc(100% - 3px); border: 0;';
