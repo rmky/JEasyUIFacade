e62d3ffcbb9cda0cf6886180d3ee933d011fc13d
aka <andrej.kabachnik@salt-solutions.de>
2019-05-23 13:00:27 +0200

    FIX phpdoc

--- Facades/Elements/EuiBrowser.php
+++ Facades/Elements/EuiBrowser.php
@@ -5,16 +5,31 @@ class EuiBrowser extends EuiAbstractElement
 {
     use HtmlBrowserTrait;
     
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\JEasyUIFacade\Facades\Elements\EuiAbstractElement::buildHtml()
+     */
     public function buildHtml()
     {
         return $this->buildHtmlIFrame();
     }
     
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\JEasyUIFacade\Facades\Elements\EuiAbstractElement::buildJs()
+     */
     public function buildJs()
     {
         return '';
     }
     
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\Core\Facades\AbstractAjaxFacade\Elements\HtmlBrowserTrait::buildCssElementStyle()
+     */
     public function buildCssElementStyle()
     {
         return 'width: 100%; height: calc(100% - 3px); border: 0;';
