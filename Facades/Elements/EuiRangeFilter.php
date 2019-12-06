15f5000ff2a3c717b3591559cc3cd8ddd47fa6f9
tmc <tmc@NBDR211.salt-solutions.de>
2019-12-06 15:58:29 +0100

    NEW added RangeFilter Element

--- Facades/Elements/EuiRangeFilter.php
+++ Facades/Elements/EuiRangeFilter.php
@@ -2,63 +2,19 @@
 namespace exface\JEasyUIFacade\Facades\Elements;
 
 use exface\Core\Widgets\RangeFilter;
-use exface\Core\Widgets\InlineGroup;
-use exface\Core\Factories\WidgetFactory;
-use exface\Core\CommonLogic\UxonObject;
+use exface\Core\Facades\AbstractAjaxFacade\Elements\JsRangeFilterTrait;
 
 /**
  * Creates and renders an InlineGroup with to and from filters.
  * 
- * @method Filter getWidget();
+ * @method RangeFilter getWidget();
  * 
  * @author Andrej Kabachnik
  *
  */
 class EuiRangeFilter extends EuiFilter
 {
-    private $inlineGroup = null;
-    
-    /**
-     * 
-     * @return InlineGroup
-     */
-    protected function getWidgetInlineGroup() : InlineGroup
-    {
-        if ($this->inlineGroup === null) {
-            $widget = $this->getWidget();
-            $wg = WidgetFactory::create($widget->getPage(), 'InlineGroup', $widget);
-            $wg->setSeparator('-');
-            
-            $inputUxon = $widget->getInputWidget()->exportUxonObject();
-            $inputUxon->setProperty('hide_caption', true);
-            $filterFromUxon = new UxonObject([
-                'widget_type' => 'Filter',
-                'hide_caption' => true,
-                'input_widget' => $inputUxon
-            ]);
-            $filterFromUxon->setProperty('comparator', $widget->getComparatorFrom());
-            $filterTo = $filterFromUxon->copy();
-            $filterTo->setProperty('comparator', $widget->getComparatorTo());
-            
-            if ($widget->hasValueFrom() === true) {
-                $filterFromUxon->setProperty('value', $widget->getValueFrom());
-            }
-            if ($widget->hasValueTo() === true) {
-                $filterTo->setProperty('value', $widget->getValueTo());
-            }
-            
-            $groupWidgets = new UxonObject([
-                $filterFromUxon,
-                $filterTo
-            ]);
-            
-            $wg->setWidgets($groupWidgets);
-            $wg->setCaption($widget->getCaption());
-            
-            $this->inlineGroup = $wg;
-        }
-        return $this->inlineGroup;
-    }
+    use JsRangeFilterTrait;
     
     /**
      * 
@@ -80,20 +36,5 @@ class EuiRangeFilter extends EuiFilter
         return $this->getFacade()->getElement($this->getWidgetInlineGroup())->buildJs();
     }
     
-    /**
-     * 
-     * @param string|null $valueJs
-     * @return string
-     */
-    public function buildJsConditionGetter($valueJs = null)
-    {
-        $conditions = [];
-        foreach ($this->getWidgetInlineGroup()->getWidgets() as $filter) {
-            $filterEl = $this->getFacade()->getElement($filter);
-            if (method_exists($filterEl, 'buildJsConditionGetter') === true) {
-                $conditions[] = $filterEl->buildJsConditionGetter($valueJs);
-            }
-        }
-        return implode(',', $conditions);
-    }
+
 }
