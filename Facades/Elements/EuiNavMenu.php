98df2ecc6e925f9e7463f1b8eb58d5705a698f17
rml <rml@NBDR326.salt-solutions.de>
2019-12-20 16:24:47 +0100

    NEW new EuiNavMenu widget

---
+++ Facades/Elements/EuiNavMenu.php
@@ -0,0 +1,65 @@
+<?php
+namespace exface\JEasyUIFacade\Facades\Elements;
+
+use exface\Core\Widgets\NavMenu;
+
+/**
+ * 
+ * @method NavMenu getWidget()
+ * 
+ * @author Andrej Kabachnik
+ *
+ */
+class EuiNavMenu extends EuiWidgetGrid 
+{
+    public function buildHtml()
+    {
+        $menu = $this->getWidget()->getMenuArray();
+        $output = <<<HTML
+
+    <!-- OWN MENU -->
+    <div class="easyui-accordion" data-options="fit:true,border:false">
+        <div title="Menü" data-options="selected:true" style="">
+            {$this->buildHtmlMenu($menu)}
+        </div>
+    </div>
+
+
+HTML;
+        return $output;
+    }
+    
+    protected function buildHtmlMenu(array $menu, int $level = 1) : string
+    {
+        
+        $exface = $this->getWorkbench();
+        if ($level === 1) {
+            $output = "<ul class='nav_menu'>";
+        } else {
+            $output = "<ul>";
+        }
+        foreach ($menu as $item) {
+            if (array_key_exists('SUB_MENU', $item)) {
+                $output .= <<<HTML
+                <li class='level{$level} active'>
+                    <a href='/exface/{$item['ALIAS']}.html'>{$item['NAME']}</a>
+{$this->buildHtmlMenu($item['SUB_MENU'], $level+1)}
+                </li>
+           
+
+HTML;
+            } else {
+                $output .= <<<HTML
+                
+                <li class='level{$level} closed'>
+                    <a href='/exface/{$item['ALIAS']}.html'>{$item['NAME']}</a>
+                </li>
+
+HTML;
+            }
+        }
+        $output .= "</ul>";
+        return $output;
+    }
+    
+}
