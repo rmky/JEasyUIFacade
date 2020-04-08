b044e361c71730d7fcd3fb547fe10a2bdac44ef1
Andrej Kabachnik <andrej_ka@gmx.de>
2020-04-08 11:35:40 +0200

    NEW submit forms on enter if there is a single promoted/normal button

--- Facades/Elements/EuiForm.php
+++ Facades/Elements/EuiForm.php
@@ -3,6 +3,7 @@ namespace exface\JEasyUIFacade\Facades\Elements;
 
 use exface\Core\Widgets\Form;
 use exface\Core\Facades\AbstractAjaxFacade\Elements\JqueryToolbarsTrait;
+use exface\Core\DataTypes\WidgetVisibilityDataType;
 
 /**
  * The Form widget is just another panel in jEasyUI.
@@ -67,5 +68,61 @@ HTML;
         
         return $options;
     }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \exface\JEasyUIFacade\Facades\Elements\EuiWidgetGrid::buildJs()
+     */
+    public function buildJs()
+    {
+        return parent::buildJs() . $this->buildJsSubmitOnEnter();
+    }
+    
+    /**
+     * Returns JS code to trigger the action of the default button of the form on enter
+     * 
+     * The default button is
+     * - the only promoted button if there is only one promoted button
+     * - the only button with normal visibility if there are no promoted buttons and only one normal one
+     * 
+     * @return string
+     */
+    protected function buildJsSubmitOnEnter()
+    {
+        $promotedButtons = [];
+        $regularButtons = [];
+        foreach ($this->getWidget()->getButtons() as $btn) {
+            if ($btn->getVisibility() == WidgetVisibilityDataType::PROMOTED) {
+                $promotedButtons[] = $btn;
+            }
+            if ($btn->getVisibility() == WidgetVisibilityDataType::NORMAL) {
+                $regularButtons[] = $btn;
+            }
+        }
+        
+        $defaultBtn = null;
+        if (count($promotedButtons) === 1) {
+            $defaultBtn = $promotedButtons[0];
+        } elseif (empty($promotedButtons) && count($regularButtons) === 1) {
+            $defaultBtn = $regularButtons[0];
+        }
+        
+        if ($defaultBtn === null) {
+            return '';
+        }
+        
+        // Use keyup() instead of keypress() because the latter did not work with jEasyUI combos.
+        return <<<JS
+        setTimeout(function(){
+            $('#{$this->getId()}').find('input').keyup(function (ev) {
+                var keycode = (ev.keyCode ? ev.keyCode : ev.which);
+                if (keycode == '13') {
+                    {$this->getFacade()->getElement($defaultBtn)->buildJsClickFunctionName()}();
+                }
+            })
+        }, 10)
+        
+JS;
+    }
 }
-?>
