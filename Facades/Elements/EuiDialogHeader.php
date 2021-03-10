f07baf86817e650781fa58db63c4d4a361f2804e
aka <aka@NBDR223.salt-solutions.de>
2021-03-10 10:53:34 +0100

    FIX height of DialogHeader was too big with widget grid

--- Facades/Elements/EuiDialogHeader.php
+++ Facades/Elements/EuiDialogHeader.php
@@ -5,6 +5,7 @@ use exface\Core\Widgets\DialogHeader;
 use exface\Core\Factories\WidgetFactory;
 use exface\Core\CommonLogic\UxonObject;
 use exface\Core\Widgets\WidgetGroup;
+use exface\Core\Facades\AbstractAjaxFacade\Elements\JqueryMasonryGridTrait;
 
 /**
  *
@@ -18,23 +19,48 @@ class EuiDialogHeader extends EuiWidgetGrid
     {
         parent::init();
         $widget = $this->getWidget();
-        $caption = $widget->getCaption();
+        $caption = $this->getCaption();
         $widget->setHideCaption(true);
         
-        $heading = WidgetFactory::createFromUxon($widget->getPage(), new UxonObject([
-            'widget_type' => 'TextHeading',
-            'text' => $widget->getMetaObject()->getName() . ' ' . $caption
-        ]), $widget);
-        
-        if ($widget->getWidgetFirst() instanceof WidgetGroup) {
-            $widget->getWidgetFirst()->addWidget($heading, 0);
-        } else {
-            $widget->addWidget($heading, 0);
+        if ($caption) {
+            $heading = WidgetFactory::createFromUxon($widget->getPage(), new UxonObject([
+                'widget_type' => 'TextHeading',
+                'text' => $caption,
+                'width' => 'max'
+            ]), $widget);
+            
+            if ($widget->getWidgetFirst() instanceof WidgetGroup) {
+                $widget->getWidgetFirst()->addWidget($heading, 0);
+            } else {
+                $widget->addWidget($heading, 0);
+            }
         }
     }
     
     public function buildCssElementClass()
     {
         return 'exf-dialog-header';
+    }
+    
+    /**
+     * After the regular grid layouter finishes, the header will adjust the size of its parent
+     * jEasyUI layout element 
+     * 
+     * @see JqueryMasonryGridTrait::buildJsLayouter()
+     */
+    public function buildJsLayouter() : string
+    {
+        return parent::buildJsLayouter() . <<<JS
+(function(){
+                var jqPanel = $('#{$this->getId()}').parents('.easyui-layout').first().layout('panel','north');
+                if (! jqPanel) return;
+                var jqGrid = jqPanel.find('.grid');
+                if (! jqGrid) return;
+                var iHeight = jqGrid.outerHeight() + 20;
+                if (jqPanel.height() > iHeight) {
+                    jqPanel.height(iHeight);
+                }
+            })();
+JS;
     }
 }
